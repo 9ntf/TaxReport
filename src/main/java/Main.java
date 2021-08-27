67e0295a2972598aa606c9c6c04d897409f45648
Alexey Khmelev <a.khmelev91@gmail.com>
2021-08-27 22:51:14 +1000

    Tax Report

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         LongAdder longAdder = new LongAdder();
-        AtomicLong sum = new AtomicLong();
 
         List<Long> shop1 = Arrays.asList(100L, 100L, 100L);
         List<Long> shop2 = Arrays.asList(200L, 200L, 200L);
@@ -14,19 +12,16 @@ public class Main {
 
         Thread thread1 = new Thread(null, () -> {
             shop1.stream().forEach(longAdder::add);
-            sum.addAndGet(longAdder.sum());
-        }, "Shop1"
+            }, "Shop1"
         );
 
         Thread thread2 = new Thread(null, () -> {
             shop2.stream().forEach(longAdder::add);
-            sum.addAndGet(longAdder.sum());
         }, "Shop2"
         );
 
         Thread thread3 = new Thread(null, () -> {
             shop3.stream().forEach(longAdder::add);
-            sum.addAndGet(longAdder.sum());
         }, "Shop2"
         );
 
@@ -38,7 +33,6 @@ public class Main {
         thread2.join();
         thread3.join();
 
-        System.out.println(sum);
-        System.out.println(longAdder.sum());
+        System.out.println("Выручка с 3х магазинов составила: " + longAdder.sum());
     }
 }
